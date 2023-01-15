8095dd25e31781b6d4b0f08b7acd93f2a4752b71
Polina Kulikova <pomidorishna@gmail.com>
2023-01-15 17:18:15 +0300

    Перед пуше1м

---
+++ dz5/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] array = { 3, 2, 1, 4, 5, 6, 3, 1 };                         // Ввела массив вручную, чтобы проверить метод умножения
+int count = array.Length / 2;
+
+
+int[] multiplication(int[] array)
+{
+        for (int i = 0; i < array.Length / 2; i++)                // Не понимаю, почему цикл не останавливается на i = 4
+        {
+             array[i] = array[i] * array[array.Length - 1 - i];
+        }
+        return array;
+
+    }
+
+
+void PrintArray(int[] collection)     // Метод печати массива
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(collection[position]);
+        position++;
+    }
+}
+
+
+int[] resultOfmultiplication = multiplication(array);
+PrintArray(array);
+
